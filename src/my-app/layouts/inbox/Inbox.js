51f682741c60be20713aca82c4dd7280bf4f6b7a
Your Name <you@example.com>
2019-01-22 01:06:40 -0800

    restore point

--- src/my-app/layouts/inbox/Inbox.js
+++ src/my-app/layouts/inbox/Inbox.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import { withStyles } from '@material-ui/core/styles';
 
+import MyMainToolbar from 'my-app/layouts/MyMainToolbar'
 import MasterDetail from '../MasterDetail'
 
 // import AcademyApp from 'my-app/apps/academy/courses/Courses';
@@ -21,11 +22,14 @@ function Inbox(props) {
   console.log('props\n', props);
   const { classes, items } = props;
   return (
-    <MasterDetail
-      // title={'Inbox Detail'}
-      items={items}
-      condensed
-    />
+    <React.Fragment>
+      <MyMainToolbar />
+      <MasterDetail
+        // title={'Inbox Detail'}
+        items={items}
+        condensed
+      />
+    </React.Fragment>
   );
 }
 
