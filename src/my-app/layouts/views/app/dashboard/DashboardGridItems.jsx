ba27e53651e6f1c58005932b26c467b6524a51a3
Your Name <you@example.com>
2019-02-06 16:20:32 -0800

    restore

--- src/my-app/layouts/views/app/dashboard/DashboardGridItems.jsx
+++ src/my-app/layouts/views/app/dashboard/DashboardGridItems.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 // import withStyles from "@material-ui/core/styles/withStyles";
 
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import AddIcon from '@material-ui/icons/Add';
 // import Button from "@material-ui/core/Button";
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
@@ -10,6 +11,7 @@ import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import ContactsIcon from '@material-ui/icons/Contacts';
 import DragHandleIcon from '@material-ui/icons/DragHandle';
 import FlagIcon from '@material-ui/icons/Flag';
+import FolderIcon from '@material-ui/icons/Folder';
 import LabelIcon from '@material-ui/icons/Label';
 import PlaceIcon from '@material-ui/icons/Place';
 import RemoveIcon from '@material-ui/icons/Remove';
