e2736da68e76e27b9daf9098bd97cd06bcf98565
Your Name <you@example.com>
2019-02-21 10:44:02 -0800

    update

--- src/my-app/layouts/Drawers/TabletDrawer.js
+++ src/my-app/layouts/Drawers/TabletDrawer.js
@@ -90,7 +90,7 @@ const styles = theme => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
 
     // temp-border
     border: 'solid purple',
