fcfd4e1cd9cb240c99e3981b55b5d7974f098990
Your Name <you@example.com>
2019-03-01 23:51:07 -0800

    update

--- src/my-app/components/ErrorMaintenance.js
+++ src/my-app/components/ErrorMaintenance.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from "@material-ui/core";
 
 import { Typography, Icon, } from '@material-ui/core';
 
-const ErrorMaintenance = () => {
+const styles = theme => ({
+  root: {
+    height: 'calc(100vh - 128px)',
+  },
+});
+
+const ErrorMaintenance = props => {
+  const { classes, } = props;
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <Icon>build</Icon>
-      <Typography className="text-20 mb-16" color="textSecondary">Temporarily down for maintenance</Typography>
+      <Typography className={classNames(classes.root, "text-40 mb-16")} color="textSecondary">Temporarily down for maintenance</Typography>
     </div>
   );
 }
- 
-export default ErrorMaintenance;
+
+export default withStyles(styles)(ErrorMaintenance);
--- src/my-app/components/Loading.js
+++ src/my-app/components/Loading.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from "@material-ui/core";
 
 import { Typography, LinearProgress, } from '@material-ui/core';
 
-const Loading = () => {
+const styles = theme => ({
+  root: {
+    height: 'calc(100vh - 128px)',
+  },
+});
+
+const Loading = props => {
+  const { classes, } = props;
   return (
-      // <React.Fragment>
-      //   <LinearProgress color="secondary" />
-      //   <Typography variant="body1">Loading...</Typography>
-      // </React.Fragment>
-      // reference: src/@fuse/components/FuseLoadable/Loading.js
-      <div className="flex flex-1 flex-col items-center justify-center">
-        <Typography className="text-20 mb-16" color="textSecondary">Loading...</Typography>
-        <LinearProgress className="w-xs" color="secondary"/>
-      </div>
+    // <React.Fragment>
+    //   <LinearProgress color="secondary" />
+    //   <Typography variant="body1">Loading...</Typography>
+    // </React.Fragment>
+    // reference: src/@fuse/components/FuseLoadable/Loading.js
+    <div className={classNames(classes.root, "flex flex-1 flex-col items-center justify-center")}>
+      <Typography className="text-20 mb-16" color="textSecondary">Loading...</Typography>
+      <LinearProgress className="w-xs" color="secondary"/>
+    </div>
   );
 }
  
-export default Loading;
+export default withStyles(styles)(Loading);
