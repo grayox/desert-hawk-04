b3ac6fd0cecc27d5c52ac56e9c951d7e48408d79
Your Name <you@example.com>
2019-03-04 07:59:43 -0800

    update

--- src/my-app/layouts/crud/CRUDContainer.js
+++ src/my-app/layouts/crud/CRUDContainer.js
@@ -35,79 +35,84 @@ class CRUDContainer extends Component {
 
   // unsubscribe = path => path && firebase.firestore().collection(path).onSnapshot(() => {})
 
+  // whileSubscribed = ( func, arg, ) => {
+  //   // if(this.state.isSubscribed) func(arg);
+  //   this.state.isSubscribed && func(arg);
+  // }
+
   getItems = path => {
     // console.log('path\n', path);
     // console.log('state\n', this.state);
     // this.setState({isLoading: true}); // infinite loop?
     
-    if(this.state.isSubscribed) {
-      console.log('state\n', this.state);
-      // debugger;
-      const out = [];
-      const db = firebase.firestore();
-      if(!db) return;
-      db.collection(path)
-        // .orderBy('added_at', 'desc')
-        // .orderBy('created_at', 'desc')
-        .orderBy('timestamp', 'desc')
-        // .limit(1)
-        .get()
-        .then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            // doc.data() is always defined for query doc snapshots
-            // console.log(doc.id, '\n', doc.data());
-            // console.log('timestamp: ', doc.timestamp()); // throws error // must define timestamp, then save it
-            // console.log('timestamp: ', doc.get('created_at')); // undefined
-            // console.log('id: ', doc.id); // works
-            // console.log('data\n', doc.data()); // works
-            out.push(doc.data());
-            // console.log('out\n', out);
-            // this.setState(out);
-          });
-          return out;
-        })
-        .then(result => {
-          // always set state inside promise!
-          // otherwise, function returns before data loads!
-          console.log('result', result);
-          const newState = {
-            path,
-            items: result,
-            isError: false,
-            isLoading: false,
-            isSubscribed: false,
-          };
-          this.setState(newState);
-          // debugger;
-          return result;
-        })
-        // .then(() => {
-        //   this.unsubscribe(path);
-        //   return path;
-        // })
-        .catch(error => {
-          console.error('Error getting documents: \n', error);
-          const newState = {
-            isError: true,
-            isLoading: false,
-            isSubscribed: false,
-          };
-          this.setState(newState);
+    // if(this.state.isSubscribed) {
+    console.log('state\n', this.state);
+    // debugger;
+    const out = [];
+    const db = firebase.firestore();
+    if(!db) return;
+    db.collection(path)
+      // .orderBy('added_at', 'desc')
+      // .orderBy('created_at', 'desc')
+      .orderBy('timestamp', 'desc')
+      // .limit(1)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          // doc.data() is always defined for query doc snapshots
+          // console.log(doc.id, '\n', doc.data());
+          // console.log('timestamp: ', doc.timestamp()); // throws error // must define timestamp, then save it
+          // console.log('timestamp: ', doc.get('created_at')); // undefined
+          // console.log('id: ', doc.id); // works
+          // console.log('data\n', doc.data()); // works
+          out.push(doc.data());
+          // console.log('out\n', out);
+          // this.setState(out);
         });
-      // console.log('out\n', out); // returns before promise settles; therefore, returns empty array
-      // always set state inside promise!
-      // otherwise, function returns before data loads!
-      // return out;
-      // const newState = { items: out };
-      // this.setState(newState);
-    }
+        return out;
+      })
+      .then(result => {
+        // always set state inside promise!
+        // otherwise, function returns before data loads!
+        console.log('result', result);
+        const newState = {
+          path,
+          items: result,
+          isError: false,
+          isLoading: false,
+          isSubscribed: false,
+        };
+        this.setState(newState);
+        // debugger;
+        return result;
+      })
+      // .then(() => {
+      //   this.unsubscribe(path);
+      //   return path;
+      // })
+      .catch(error => {
+        console.error('Error getting documents: \n', error);
+        const newState = {
+          isError: true,
+          isLoading: false,
+          isSubscribed: false,
+        };
+        this.setState(newState);
+      });
+    // console.log('out\n', out); // returns before promise settles; therefore, returns empty array
+    // always set state inside promise!
+    // otherwise, function returns before data loads!
+    // return out;
+    // const newState = { items: out };
+    // this.setState(newState);
+    // }
   };
 
   Child = /* async */ ({ match: { params: { id }}}) => {
     // console.log('id\n', id);
     // if(!this.state.isLoading) this.setState({isLoading: true});
-    const { getItems } = this;
-    const { items, isLoading, isError, } = this.state; // isSubscribed,
+    const { getItems, } = this; // whileSubscribed,
+    const { items, isLoading, isError, isSubscribed, } = this.state;
     const matches = componentsNavConfig.filter(r => (r.id === id));
     const item = matches[0];
     const config = item.crudConfig;
@@ -119,7 +124,9 @@ class CRUDContainer extends Component {
     // this.setState({ path, });
     // this.unsubscribe(await getItems(path));
     // if(!isSubscribed) return;
-    getItems(path);
+    // getItems(path);
+    // whileSubscribed( getItems, path, );
+    isSubscribed && getItems(path);
     // console.log('items\n', items);
     return (
       isLoading
