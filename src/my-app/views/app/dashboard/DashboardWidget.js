d8e4d8c61c38c53de2c8365750f3f4dc1105de8d
Your Name <you@example.com>
2019-04-09 19:52:13 -0700

    update

--- src/my-app/views/app/dashboard/DashboardWidget.js
+++ src/my-app/views/app/dashboard/DashboardWidget.js
@@ -59,7 +59,7 @@ class DashboardWidget extends Component {
           </div>
           <div className="text-center pt-12 pb-28">
             <Typography className="text-72 leading-none text-blue">{data}</Typography>
-            <Typography className="text-16" color="textSecondary">{label}</Typography>
+            <Typography className="text-xs uppercase" color="textSecondary">{label}</Typography>
           </div>
           <div className="flex items-center px-16 h-52 border-t-1 border-grey-light">
             <Typography className="text-15 flex w-full" color="textSecondary">
