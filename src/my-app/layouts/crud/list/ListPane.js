53091e6ced39d0858cf13ef456d4b075df4c7456
Your Name <you@example.com>
2019-05-24 02:20:04 -0700

    u

--- src/my-app/layouts/crud/list/ListPane.js
+++ src/my-app/layouts/crud/list/ListPane.js
@@ -19,69 +19,75 @@ const styles = theme => ({
 const ListPane = ({
   classes, items, onNext, onToggle, hasMore, creatable, searchable, filterable, sortable,
   handleOpenSearch, handleOpenFilter, handleOpenSort, handleOpenCreateDialog,
-}) => (
-  <React.Fragment>
-    <div className="w-full">
-      <Zoom in mountOnEnter unmountOnExit>
-        <ButtonsTier
-          creatable={creatable}
-          searchable={searchable}
-          filterable={filterable}
-          sortable={sortable}
-          onClickCreate={handleOpenCreateDialog}
-          onClickSearch={handleOpenSearch}
-          onClickFilter={handleOpenFilter}
-          onClickSort={handleOpenSort}
-        />
-      </Zoom>
-    </div>
+}) => {
+  const ready1 = items && items.length;
+  if(!ready1) return null;
   
-    <Paper className={classNames(classes.paper, "z-10",)}>
-      <List className="m-0 p-0" component="nav">
-        {
-          // subheader={<ListSubheader className="text-left">Items</ListSubheader>}
-        }
-        <InfiniteScroll
-          dataLength={items.length}
-          next={onNext} // event
-          hasMore={hasMore} // boolean
-          loader={
-            // {<h4>Loading...</h4>}
-            <div className='ml-8'>
-              <CircularProgress className={classes.progress} color="secondary" />
-            </div>
+  return (
+    <React.Fragment>
+      <div className="w-full">
+        <Zoom in mountOnEnter unmountOnExit>
+          <ButtonsTier
+            creatable={creatable}
+            searchable={searchable}
+            filterable={filterable}
+            sortable={sortable}
+            onClickCreate={handleOpenCreateDialog}
+            onClickSearch={handleOpenSearch}
+            onClickFilter={handleOpenFilter}
+            onClickSort={handleOpenSort}
+          />
+        </Zoom>
+      </div>
+    
+      <Paper className={classNames(classes.paper, "z-10",)}>
+        <List className="m-0 p-0" component="nav">
+          {
+            // subheader={<ListSubheader className="text-left">Items</ListSubheader>}
           }
-          height={window.innerHeight - 128 - 28} // {800} {400}
-          endMessage={
-            <div className="text-center p-16">
-              End of list
-            </div>
-          }
-        >
-          <FuseAnimateGroup
-            delay={500}
-            enter={{ animation: "transition.slideUpBigIn" }}
-            leave={{ animation: "transition.slideLeftOut" }}
-          >
-            {
-              items && items.map( ( item, index, ) =>
-                <Tooltip key={item && item.createdAt} TransitionComponent={Zoom} placement="top" title="Click for detail">
-                  <div
-                    // className="border-b" // use divider instead
-                  >
-                    {/* { getSummary( item, true, index, ) } */}
-                    <ItemSummary item={item} side="list" index={index} onToggle={onToggle} />
-                    <Divider />
-                  </div>
-                </Tooltip>
-              )
+          <InfiniteScroll
+            dataLength={items && items.length}
+            next={onNext} // event
+            hasMore={hasMore} // boolean
+            loader={
+              // {<h4>Loading...</h4>}
+              <div className='ml-8'>
+                <CircularProgress className={classes.progress} color="secondary" />
+              </div>
             }
-          </FuseAnimateGroup>
-        </InfiniteScroll>
-      </List>
-    </Paper>
-  </React.Fragment>
-)
+            height={window.innerHeight - 128 - 28} // {800} {400}
+            endMessage={
+              <div className="text-center p-16">
+                End of list
+              </div>
+            }
+          >
+            <FuseAnimateGroup
+              delay={500}
+              enter={{ animation: "transition.slideUpBigIn" }}
+              leave={{ animation: "transition.slideLeftOut" }}
+            >
+              {
+                items && items.map( ( item, index, ) =>
+                  <Tooltip key={item && item.createdAt} TransitionComponent={Zoom} placement="top" title="Click for detail">
+                    <div
+                      // className="border-b" // use divider instead
+                    >
+                      {/* { getSummary( item, true, index, ) } */}
+                      <ItemSummary item={item} side="list" index={index} onToggle={onToggle} />
+                      <Divider />
+                    </div>
+                  </Tooltip>
+                )
+              }
+            </FuseAnimateGroup>
+          </InfiniteScroll>
+        </List>
+      </Paper>
+    </React.Fragment>
+  )
+}
+
 
 // export default ListPane;
 export default withStyles(styles)(ListPane);
