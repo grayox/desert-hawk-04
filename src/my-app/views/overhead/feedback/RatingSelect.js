dae9c38682e1ace402c71a9f625be29c1dfef695
Your Name <you@example.com>
2019-05-29 02:38:20 -0700

    u

--- src/my-app/views/overhead/feedback/RatingSelect.js
+++ src/my-app/views/overhead/feedback/RatingSelect.js
@@ -16,7 +16,7 @@ import Rating from 'react-rating';
 // const styles = theme => ({});
 
 // eslint-disable-next-line
-const THANKS_MESSAGE = 'Thanks! 👼 Please consider also rating us at';
+const THANKS_MESSAGE = 'Thanks! 👼 Would you also like to rate us at';
 const FEEDBACK_REQUEST = 'Feedback submitted. Thank you. 🙏\n\nPlease consider sending us a note about how we can improve.';
 
 const RatingSelect = props => {
@@ -54,43 +54,35 @@ const RatingSelect = props => {
     alert(`${THANKS_MESSAGE} ${redirectPath}`);
   }
   
-  return (
-
-    <Card className="w-full m-0 md:mb-16">
-      
-      <AppBar position="static" elevation={0}>
-        <Toolbar className="pl-16 pr-8">
-          <Typography variant="subtitle1" color="inherit" className="flex-1">
-            {heading}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-    {
-    // <Paper className="max-w-sm m-32 p-32">
-    //   <Typography className="h1 mb-36">{heading}</Typography>
-      
-      // <Typography className="body1 mb-24">{value}</Typography>
-      // <Rating fractions={2} />
-    }
-
-      <CardContent className="p-32">
-        <Rating
-          stop={stop} initialRating={value} onClick={handleClick} // onChange={handleChange}
-          emptySymbol={<FaRegStar className="mx-8 text-3xl text-orange" />}
-          fullSymbol={<FaStar className="mx-8 text-3xl text-orange" />}
-        />
-        <Typography className="ml-12 opacity-50" hidden={!value} >
-          {value && `${value}/5 stars`}
+  const getAppBar = () =>
+    <AppBar position="static" elevation={0}>
+      <Toolbar className="pl-16 pr-8">
+        <Typography variant="subtitle1" color="inherit" className="flex-1">
+          {heading}
         </Typography>
-        <div className="mt-24 text-right">
-          <Button disabled={disabledButtons} onClick={handleReset}>Reset</Button>
-          <Button variant="contained" color="secondary" disabled={disabledButtons} onClick={handleSave}>Submit</Button>
-        </div>
-    {/* </Paper> */}
-      </CardContent>
-    </Card>
-  );
+      </Toolbar>
+    </AppBar>
+
+  const getRating = () =>
+    <Rating
+      stop={stop} initialRating={value} onClick={handleClick} // onChange={handleChange}
+      emptySymbol={<FaRegStar className="mx-8 text-3xl text-orange" />}
+      fullSymbol={<FaStar className="mx-8 text-3xl text-orange" />}
+    />
+
+  const getValue = () => <Typography className="ml-12 opacity-50" hidden={!value}>{value && `${value}/5 stars`}</Typography>
+  
+  const getButtons = () => 
+    <div className="mt-24 text-right">
+      <Button disabled={disabledButtons} onClick={handleReset}>Reset</Button>
+      <Button variant="contained" color="secondary" disabled={disabledButtons} onClick={handleSave}>Submit</Button>
+    </div>
+
+  const getCardContent = () =><CardContent className="p-32">{getRating()}{getValue()}{getButtons()}</CardContent>
+
+  const getRatingSelect = () => <Card className="w-full m-0 md:mb-16">{getAppBar()}{getCardContent()}</Card>
+
+  return getRatingSelect();
 }
 
 RatingSelect.defaultProps = {
