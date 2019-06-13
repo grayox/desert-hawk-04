7c64385fd6f92bef265d87d8e8545fc3b2a00269
Your Name <you@example.com>
2019-06-13 02:00:27 -0700

    u

--- src/my-app/views/app/dashboard/WidgetNugget.js
+++ src/my-app/views/app/dashboard/WidgetNugget.js
@@ -2,6 +2,7 @@
 
 import React, { useState, } from 'react';
 import _ from '@lodash';
+import numeral from 'numeral';
 import {
   Button, Slide, Chip, // Typography, // withStyles
   Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
@@ -10,16 +11,19 @@ import {
 const computeFontSize = ( data = '' ) => {  
 
   const fontSizeLookupTable = () => {
+    // some values of 'text-x' work, some don't
+    // work: text-72 70 68 64 56 48 40 36 32 28 24 20 18 16
+    // don't work: text-30
     let a = [];
     a.length = 32;
-    a.fill( 'text-72'        );
-    a.fill( 'text-68' ,  4 , );
-    a.fill( 'text-64' ,  5 , );
-    a.fill( 'text-48' ,  6 , );
-    a.fill( 'text-40' ,  7 , );
-    a.fill( 'text-36' ,  8 , );
-    a.fill( 'text-32' ,  9 , );
-    a.fill( 'text-30' , 10 , );
+    a.fill( 'text-72'        ); // alt
+    a.fill( 'text-68' ,  4 , ); // 70
+    a.fill( 'text-64' ,  5 , ); // 68
+    a.fill( 'text-48' ,  6 , ); // 64 // too big for 'Canada'
+    a.fill( 'text-40' ,  7 , ); // 56
+    a.fill( 'text-36' ,  8 , ); // 48
+    a.fill( 'text-32' ,  9 , ); // 36
+  //a.fill( 'text-32' , 10 , ); // used to be 'text-30', but 'text-30' didn't render as expected (it was too small -- same as undefined)
     a.fill( 'text-28' , 12 , );
     a.fill( 'text-24' , 14 , );
     a.fill( 'text-20' , 16 , );
@@ -33,10 +37,16 @@ const computeFontSize = ( data = '' ) => {
 
   const newString = _.toString(data);
   const stringLength = newString.length;
+
+  // account for commas
+  const numberOfCommas = newString.split(',').length - 1; // count number of commas
+  const adjLength = stringLength - numberOfCommas;
   
-  // console.log('stringLength\n', stringLength,);
+  // console.log('data\n', data,);
+  // console.log('newString\n', newString,);
+  // console.log('adjLength\n', adjLength,);
   // console.log('lookupTableLength\n', lookupTableLength,);
-  const out = (stringLength >= lookupTableLength) ? 'text-16' : lookupTable[stringLength];
+  const out = (adjLength >= lookupTableLength) ? 'text-16' : lookupTable[adjLength];
   // console.log('out\n', out,);
   return out;
 }
@@ -49,6 +59,9 @@ const Transition = props => <Slide direction="up" {...props} />
 // const WidgetNugget = props => <Chip className={classes.chip} />
 // const WidgetNugget = ({ label, message, }) => <Chip label={label} onClick={() => handleClick(message)} />
 const WidgetNugget = ({ type, data, label, message, }) => {
+
+  const formattedData = typeof data === 'number' ? numeral(data).format('0,0') : data;
+
   const config = {
     chip: {
       // direction: 'up',
@@ -63,8 +76,8 @@ const WidgetNugget = ({ type, data, label, message, }) => {
           className="flex flex-col h-128 text-center pb-12 cursor-pointer"
           onClick={() => handleOpenDialog()}
         >
-          <div className={`${computeFontSize(data)} h-72 flex items-end leading-none text-blue`}>
-            <span className="flex-1">{data}</span>
+          <div className={`${computeFontSize(formattedData)} h-72 flex items-end leading-none text-blue`}>
+            <span className="flex-1">{formattedData}</span>
           </div>
           <div className="text-xs uppercase mt-16" color="textSecondary">{label}</div>
         </div>
@@ -72,35 +85,36 @@ const WidgetNugget = ({ type, data, label, message, }) => {
     },
   };
 
+  const getDialog = () =>
+    <Dialog
+      keepMounted
+      open={dialogIsOpen}
+      onClose={handleCloseDialog}
+      TransitionComponent={Transition}
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle id="alert-dialog-slide-title">{label}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        {
+        // <Button onClick={handleCloseDialog} color="primary">Disagree</Button>
+        // <Button onClick={handleCloseDialog} color="primary">Agree</Button>
+        }
+        <Button onClick={handleCloseDialog} color="primary">Ok, got it</Button>
+      </DialogActions>
+    </Dialog>
+
   const [ dialogIsOpen, setDialogIsOpen, ] = useState(false);
   const handleOpenDialog = () => setDialogIsOpen(true);
   const handleCloseDialog = () => setDialogIsOpen(false);
   // const Transition = props => (<Slide direction={config[type].direction} {...props} />);
-  return (
-    <div>
-      {config[type].element}
-      <Dialog
-        keepMounted
-        open={dialogIsOpen}
-        onClose={handleCloseDialog}
-        TransitionComponent={Transition}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle id="alert-dialog-slide-title">{label}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">{message}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          {
-          // <Button onClick={handleCloseDialog} color="primary">Disagree</Button>
-          // <Button onClick={handleCloseDialog} color="primary">Agree</Button>
-          }
-          <Button onClick={handleCloseDialog} color="primary">Ok, got it</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+
+  const getWidgetNugget = () => <div> {config[type].element} {getDialog()} </div>
+
+  return getWidgetNugget();
 }
 
 export default WidgetNugget;
