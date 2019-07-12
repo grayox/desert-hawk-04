8f37d49c0ea46e2b6c0ef9f447442356b15b80db
Your Name <you@example.com>
2019-07-11 21:54:48 -0700

    u

--- src/my-app/views/overhead/Logout.js
+++ src/my-app/views/overhead/Logout.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { connect, } from 'react-redux';
 import { Redirect, } from 'react-router-dom';
-import firebaseService from 'firebaseService';
+// import firebaseService from 'firebaseService';
+import { logoutUser, } from 'auth/store/actions/user.actions';
 
-const Logout = () => {
-  console.log('Logging out...');
-  alert('Logging out...');
-  // TODO: add logout functionality
+const Logout = props => {
+  // console.log('Logging out...');
+  // alert('Logging out...');
+  props.logoutUser();
+  // add logout functionality
   // firebaseService.signOut();
   return (
     <Redirect to='/login' />
   );
 }
+
+const mapDispatchToProps = dispatch => ({
+  logoutUser: () => dispatch(logoutUser()),
+})
  
-export default Logout;
+// export default Logout;
+export default connect( null, mapDispatchToProps, )(Logout);
