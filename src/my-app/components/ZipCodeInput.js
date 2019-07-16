3cfddf02592195c88bc9b13a8df9919f55cab994
Your Name <you@example.com>
2019-07-16 02:08:03 -0700

    u

--- src/my-app/components/ZipCodeInput.js
+++ src/my-app/components/ZipCodeInput.js
@@ -4,47 +4,53 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, TextField, Icon, } from '@material-ui/core';
+import { withStyles, TextField, Icon, InputAdornment, } from '@material-ui/core';
 import zipCodeData from 'my-app/components/GeoSelect/zip-code-data';
 import _ from '@lodash';
 
 class ZipCodeInput extends Component {
   
   state = { 
-    valid  : false ,
-    zip    : ''    ,
-    lat    : ''    ,
-    lon    : ''    ,
-    city   : ''    ,
-    state  : ''    ,
-    county : ''    ,
+    isValid : false ,
+    zip     : ''    ,
+    lat     : ''    ,
+    lon     : ''    ,
+    city    : ''    ,
+    state   : ''    ,
+    county  : ''    ,
   }
 
   setValid = () => {
     const { zip, } = this.state;
+    const { onValid, } = this.props;
     const zipData = zipCodeData[zip];
     // console.log('zipData\n', zipData,);
 
-    const valid = zipData && !_.isEmpty(zipData);
-    const ready1 = valid;
+    const isValid = zipData && !_.isEmpty(zipData);
+    const ready1 = isValid;
     if(!ready1) return null;
 
     // const { lat, lon, city, state, county, } = zipData;
     this.setState({
-      valid, ...zipData, // lat, lon, city, state, county,
-    });
+      isValid, ...zipData, // lat, lon, city, state, county,
+    },
+      () => {
+        if(isValid) onValid(this.state);
+      }
+    );
   }
 
   handleChange = event => {
-    // console.log('target\n', event.target);
     const TARGET_LENGTH = 5; // nominal length of zip code string, e.g., 10118
+    
+    // console.log('target\n', event.target);
     const { value, } = event.target; // id,
     const length = value && value.length;
 
-    const ready1 = length < (TARGET_LENGTH + 1); // 6
+    const ready1 = length < ( TARGET_LENGTH + 1 ); // 6
     if(!ready1) return null;
 
-    if ( length < TARGET_LENGTH ) this.setState({ valid: false, });
+    if ( length < TARGET_LENGTH ) this.setState({ isValid: false, });
     
     this.setState({ zip: value, }, () => {
       if( length === TARGET_LENGTH ) this.setValid();
@@ -52,9 +58,9 @@ class ZipCodeInput extends Component {
   }
 
   render() {
-    const { icon, } = this.props;
+    const { icon, isRequired, } = this.props; // onValid,
     const { handleChange, } = this;
-    const { valid, zip, city, state, county, } = this.state; // lat, lon,
+    const { isValid, zip, city, state, county, } = this.state; // lat, lon,
 
     return (
       <div className="flex">
@@ -64,7 +70,7 @@ class ZipCodeInput extends Component {
         <TextField
           // className={classes.formControl}
           className="mb-24"
-          label="Zip code" // {label}
+          label={`Zip code${isRequired ? ' *' : ''}`} //"Zip code" // {label}
           // autoFocus={autoFocus}
           // id="zip-code-input"
           // name={id}
@@ -81,8 +87,25 @@ class ZipCodeInput extends Component {
           // rows={rows}
           // InputLabelProps={InputLabelProps}
 
-          helperText={valid ? `${city}, ${county} County, ${state}` : ''} // ${lat} ${lon} ${valid} // "Some important text"
+          helperText={isValid ? `${city}, ${county} County, ${state}` : ''} // ${lat} ${lon} ${isValid} // "Some important text"
           margin="normal"
+
+          // InputProps={{
+          //   endAdornment: (
+          //     <InputAdornment position="end">
+          //       {
+          //       // <IconButton
+          //       //   edge="end"
+          //       //   aria-label="Toggle password visibility"
+          //       //   onClick={handleClickShowPassword}
+          //       // >
+          //       //   {values.showPassword ? <VisibilityOff /> : <Visibility />}
+          //       // </IconButton>
+          //       }
+          //       <Icon>{ isValid ? 'done' : 'clear' }</Icon>
+          //     </InputAdornment>
+          //   ),
+          // }}
         />
       </div>
     );
@@ -91,10 +114,14 @@ class ZipCodeInput extends Component {
 
 ZipCodeInput.propTypes = {
   icon: PropTypes.string,
+  isRequired: PropTypes.bool,
+  onValid: PropTypes.func,
 };
 
 ZipCodeInput.defaultProps = {
   icon: 'place',
+  isRequired: true,
+  onValid: () => console.log('is valid'),
 };
  
 export default ZipCodeInput;
